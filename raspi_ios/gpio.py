9d0b29f1cef7fa99b01ae898721aee44a47f7f50
wxidong@Sanai.win7 <amaork@gmail.com>
2018-02-09 19:13:34 +0800

    add SoftSPI(GPIO simulate SPI Controller) support

--- raspi_ios/gpio.py
+++ raspi_ios/gpio.py
@@ -2,7 +2,8 @@
 import uuid
 import RPi.GPIO as GPIO
 from .core import RaspiIOHandle
-from raspi_io.gpio import GPIOMode, GPIOSetup, GPIOCleanup, GPIOCtrl, GPIOChannel, GPIOSoftPWM, GPIOSoftPWMCtrl
+from raspi_io.gpio import GPIOMode, GPIOSetup, GPIOCleanup, GPIOCtrl, GPIOChannel, \
+    GPIOSoftPWM, GPIOSoftPWMCtrl, GPIOSoftSPI, GPIOSoftSPIXfer, GPIOSoftSPIRead, GPIOSoftSPIWrite
 __all__ = ['RaspiGPIOHandle']
 
 
@@ -16,6 +17,7 @@ class RaspiGPIOHandle(RaspiIOHandle):
         GPIO.setwarnings(False)
         self.__io_res = set()
         self.__pwm_list = dict()
+        self.__spi_list = dict()
 
     def __del__(self):
         [pwm.stop() for pwm in self.__pwm_list.values()]
@@ -36,6 +38,7 @@ class RaspiGPIOHandle(RaspiIOHandle):
             for channel in gpio:
                 self.check_gpio(channel)
         else:
+            # TODO: gpio same directions is ok
             if gpio in self.IO_RES and gpio not in self.__io_res:
                 raise IOError("Channel:{} is occupied".format(gpio))
 
@@ -113,3 +116,113 @@ class RaspiGPIOHandle(RaspiIOHandle):
 
         # Start or stop pwm, duty == 0 stop pwm
         pwm.start(ctrl.duty) if ctrl.duty else pwm.stop()
+
+    async def spi_init(self, ws, data):
+        spi = GPIOSoftSPI(**data)
+
+        # Get pwm instance uuid
+        spi_uuid = spi.generate_uuid()
+
+        # Init spi gpio
+        GPIO.setmode(spi.mode)
+        # TODO: Check gpio
+        # self.check_gpio([spi.cs, spi.clk, spi.mosi, spi.miso])
+        GPIO.setup(spi.miso, GPIO.IN)
+        GPIO.setup([spi.cs, spi.clk, spi.mosi], GPIO.OUT)
+
+        # Register spi to spi device list
+        self.__spi_list[spi_uuid] = spi
+
+    async def spi_xfer(self, ws, data):
+        xfer = GPIOSoftSPIXfer(**data)
+
+        # Get spi instance
+        spi = self.__spi_list.get(xfer.uuid)
+        if not isinstance(spi, GPIOSoftSPI):
+            raise ValueError("Do not found spi:{}".format(xfer.uuid))
+
+        read_bytes = list()
+        shift_mask = (1 << (spi.bits_per_word - 1))
+        word_mask = 0xffffffffffffffff >> (64 - spi.bits_per_word)
+
+        # SPI bus init
+        GPIO.output(spi.cs, 1)
+        GPIO.output([spi.cs, spi.clk, spi.mosi], 0)
+
+        # Write data to spi
+        for word in xfer.data:
+            word &= word_mask
+            for i in range(spi.bits_per_word):
+                GPIO.output(spi.clk, 0)
+                GPIO.output(spi.mosi, 1 if word & shift_mask else 0)
+                GPIO.output(spi.clk, 1)
+                word = (word << 1) & word_mask
+
+        # Read data back
+        for n in range(xfer.size):
+            word = 0x0
+            for i in range(spi.bits_per_word):
+                GPIO.output(spi.clk, 0)
+                bit = GPIO.input(spi.miso) & 1
+                word |= (bit << (spi.bits_per_word - 1 - i))
+                GPIO.output(spi.clk, 1)
+
+            read_bytes.append(word)
+
+        return read_bytes
+
+    async def spi_read(self, ws, data):
+        read = GPIOSoftSPIRead(**data)
+
+        # Get spi instance
+        spi = self.__spi_list.get(read.uuid)
+        if not isinstance(spi, GPIOSoftSPI):
+            raise ValueError("Do not found spi:{}".format(read.uuid))
+
+        # SPI bus init
+        read_bytes = list()
+        GPIO.output(spi.cs, 1)
+        GPIO.output([spi.cs, spi.clk, spi.mosi], 0)
+
+        # Read data back
+        for n in range(read.size):
+            word = 0x0
+            for i in range(spi.bits_per_word):
+                GPIO.output(spi.clk, 0)
+                bit = GPIO.input(spi.miso) & 1
+                word |= (bit << (spi.bits_per_word - 1 - i))
+                GPIO.output(spi.clk, 1)
+
+            read_bytes.append(word)
+
+        return read_bytes
+
+    async def spi_write(self, ws, data):
+        write = GPIOSoftSPIWrite(**data)
+
+        # Get spi instance
+        spi = self.__spi_list.get(write.uuid)
+        if not isinstance(spi, GPIOSoftSPI):
+            raise ValueError("Do not found spi:{}".format(write.uuid))
+
+        # TODO: Move to GPIOSoftSPI class
+        shift_mask = (1 << (spi.bits_per_word - 1))
+        word_mask = 0xffffffffffffffff >> (64 - spi.bits_per_word)
+
+        # SPI bus init
+        GPIO.output(spi.cs, 1)
+        GPIO.output([spi.cs, spi.clk, spi.mosi], 0)
+
+        # Write data to spi
+        for word in write.data:
+            word &= word_mask
+            for i in range(spi.bits_per_word):
+                GPIO.output(spi.clk, 0)
+                GPIO.output(spi.mosi, 1 if word & shift_mask else 0)
+
+                # LSB Strobe
+                if i == spi.bits_per_word - 1:
+                    GPIO.output(spi.cs, 1)
+
+                GPIO.output(spi.clk, 1)
+                word = (word << 1) & word_mask
