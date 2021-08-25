f09dc3607c90b60d27058dbf6897fb8ba449dcd2
amaork@Home_WIN7 <amaork@gmail.com>
2021-08-25 10:46:15 +0800

    fix release script bugs

--- release.py
+++ release.py
@@ -11,9 +11,15 @@ from raspi_ios.version import version
 if __name__ == '__main__':
     dist = 'dist'
     name = 'raspi_io_server'
+    path = os.path.join(dist, name)
+    package_name = '{}_release_{}.tar'.format(name, version)
+
+    readme = 'readme'
     boot_script = 'boot_apps.sh'
     release_file = 'release.json'
-    path = os.path.join(dist, name)
+    package_file_list = (name, readme, boot_script, release_file)
+    rc_local_launch_cmd = 'mkdir /tmp/rios && cp /usr/local/sbin/raspi_io_server ' \
+                          '/tmp/rios && cd /tmp/rios && ./raspi_io_server && /usr/local/sbin/boot_apps.sh'
 
     try:
         desc = dict(
@@ -26,9 +32,11 @@ if __name__ == '__main__':
             url=""
         )
 
+        # Generate release.json
         with open(os.path.join(dist, release_file), 'w', encoding='utf-8') as fp:
             json.dump(desc, fp, indent=4)
 
+        # Generate boot_args.sh
         with open(os.path.join(dist, boot_script), 'w', encoding='utf-8') as fp:
             fp.write('#!/bin/sh\n\n')
             fp.write('root=/usr/local/sbin/scripts\n')
@@ -42,10 +50,18 @@ if __name__ == '__main__':
         os.chmod(path=os.path.join(dist, boot_script),
                  mode=stat.S_IXUSR | stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH | stat.S_IWRITE)
 
-        with tarfile.open(os.path.join(dist, '{}_release_{}.tar'.format(name, version)), 'w:tar') as tar:
-            tar.add(os.path.join(dist, name))
-            tar.add(os.path.join(dist, boot_script))
-            tar.add(os.path.join(dist, release_file))
+        # Generate readme
+        with open(os.path.join(dist, readme), 'w', encoding='utf-8') as fp:
+            fp.write("1. first extract {!r} to '/usr/local/sbin'\n".format(package_name))
+            fp.write("2. then add {!r} to '/etc/rc.local'\n".format(rc_local_launch_cmd))
 
+        # Pack to release package
+        pwd = os.getcwd()
+        os.chdir(dist)
+        with tarfile.open(package_name, 'w:tar') as tar:
+            for file in package_file_list:
+                tar.add(file)
+
+        os.chdir(pwd)
     except (OSError, ValueError, AttributeError) as e:
         print("Generate {!r} release desc failed: {}".format(path, e))
